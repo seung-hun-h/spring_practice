52647c8cfc8ee650a0a6c97f68867713e800def2
seung-h <dau4723@gmail.com>
2021-06-02 14:49:03 +0900

    Spring Jpa

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,30 @@
 package hello.hellospring;
 
-import hello.hellospring.repogitory.JdbcTemplateMemberRepogitory;
+import hello.hellospring.repogitory.JpaMemberRepogitory;
 import hello.hellospring.repogitory.MemberRepogitory;
-import hello.hellospring.repogitory.MemoryMemberRepogitory;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+//    private final DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
-
 
     @Bean
     public MemberService memberService() {
@@ -29,7 +34,9 @@ public class SpringConfig {
     @Bean
     public MemberRepogitory memberRepogitory() {
 //        return new MemoryMemberRepogitory();
-        return new JdbcTemplateMemberRepogitory(dataSource);
+//        return new JdbcTemplateMemberRepogitory(dataSource);
+        return new JpaMemberRepogitory(em);
     }
+
 
 }
