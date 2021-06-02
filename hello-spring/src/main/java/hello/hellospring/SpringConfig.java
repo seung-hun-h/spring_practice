ec4aed25deaee517d21e4b18c9c96e648c0314c9
seung-h <dau4723@gmail.com>
2021-06-02 13:56:26 +0900

    Spring Jdbc Template

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repogitory.JdbcTemplateMemberRepogitory;
 import hello.hellospring.repogitory.MemberRepogitory;
 import hello.hellospring.repogitory.MemoryMemberRepogitory;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepogitory memberRepogitory() {
-        return new MemoryMemberRepogitory();
+//        return new MemoryMemberRepogitory();
+        return new JdbcTemplateMemberRepogitory(dataSource);
     }
+
 }
