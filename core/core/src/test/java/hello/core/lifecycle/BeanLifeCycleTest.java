e496a637a0e293a758658670681f4c56ade10859
seung-h <dau4723@gmail.com>
2021-06-25 13:16:19 +0900

    chapter8 빈 생명주기 콜백

---
+++ core/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.JavaBean;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close(); // 스프링 테이너 종료
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+//        @Bean(initMethod = "init", destroyMethod = "close") //destroyMethod 생략 가능 -> 메서드 이름을 통한 추론
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+
+            return networkClient;
+        }
+    }
+
+}
