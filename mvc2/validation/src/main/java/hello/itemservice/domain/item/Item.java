f1b824b99bf554b3bd9db466605aed8655a59be6
seunghun-h <dau4723@gmail.com>
2022-01-06 21:27:20 +0900

    rest validation

--- mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,21 +8,11 @@ import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
