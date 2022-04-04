3f52383268cad5b0184f8f515cd3ec7363e9275c
seung-hun-h <dau4723@gmail.com>
2022-04-04 14:55:46 +0900

    vue cli basic

--- learn-vue-js/vue-cli/src/App.vue
+++ learn-vue-js/vue-cli/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <AppHeader v-bind:propsdata="str" v-on:renew="renewStr"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  methods: {
+    renewStr: function(newValue) {
+      this.str = newValue;
+    }
+  },
   components: {
-    HelloWorld
+    AppHeader,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ learn-vue-js/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <h1>
+          {{ propsdata }}
+      </h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: {
+        propsdata: String
+    },
+    methods: {
+        sendEvent: function() {
+            this.$emit('renew', 'hi');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
