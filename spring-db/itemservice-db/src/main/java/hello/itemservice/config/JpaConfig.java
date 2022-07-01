33dc03191dcabe4f3a14be57d19442493309548b
seunghun <dau4723@gmail.com>
2022-07-01 09:59:54 +0900

    jpa

---
+++ spring-db/itemservice-db/src/main/java/hello/itemservice/config/JpaConfig.java
@@ -0,0 +1,30 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+@Configuration
+public class JpaConfig {
+    private final EntityManager em;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV1(em);
+    }
+}
