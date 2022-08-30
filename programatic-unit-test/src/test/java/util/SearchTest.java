91f31839fa4a4b3218e4c53d34404e9c31900aee
seunghun <dau4723@gmail.com>
2022-08-30 09:41:25 +0900

    11.7 테스트 냄새: 테스트와 무관한 세부 사항들

--- programatic-unit-test/src/test/java/util/SearchTest.java
+++ programatic-unit-test/src/test/java/util/SearchTest.java
@@ -6,55 +6,61 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 import java.util.logging.Level;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SearchTest {
 
 	public static final String A_TITLE = "1";
+	private InputStream stream;
+
+	@BeforeEach
+	void turnOffLogging() {
+		Search.LOGGER.setLevel(Level.OFF);
+	}
+
+	@AfterEach
+	void closeResources() throws IOException {
+		stream.close();
+	}
 
 	@Test
 	public void returnsMatchesShowingContextWhenSearchStringInContent() throws Exception {
-		ByteArrayInputStream stream = streamOn("There are certain queer times and occasions "
+		stream = streamOn("There are certain queer times and occasions "
 			+ "in this strange mixed affair we call life when a man "
 			+ "takes this whole universe for a vast practical joke, "
 			+ "though the wit thereof he but dimly discerns, and more "
 			+ "than suspects that the joke is at nobody's expense but "
 			+ "his own.");
 
-		// search
 		Search search = new Search(stream, "practical joke", A_TITLE);
-		Search.LOGGER.setLevel(Level.OFF);
 		search.setSurroundingCharacterCount(10);
 		search.execute();
-		assertFalse(search.errored());
-		assertThat(search.getMatches(),
-			ContainsMatches.containsMatches(
-				new Match[] {
-					new Match(A_TITLE, "practical joke", "or a vast practical joke, through t")
-				}
-			));
-		stream.close();
-
+		assertThat(search.getMatches(), ContainsMatches.containsMatches(
+			new Match[] {
+				new Match(A_TITLE, "practical joke", "or a vast practical joke, though t")
+			}
+		));
 	}
 
 	@Test
 	void noMatchesReturnedWhenSearchStringNotInContent() throws IOException {
-		// negative
 		URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
-		InputStream inputStream = connection.getInputStream();
-		Search search = new Search(inputStream, "smelt", A_TITLE);
+		stream = connection.getInputStream();
+		Search search = new Search(stream, "smelt", A_TITLE);
 		search.execute();
 		assertTrue(search.getMatches().isEmpty());
-		inputStream.close();
+		stream.close();
 	}
 
-	
+
 	private ByteArrayInputStream streamOn(String pageContent) {
 		return new ByteArrayInputStream(pageContent.getBytes());
 	}
