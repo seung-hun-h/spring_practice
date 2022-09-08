bbb6f0a6522b008fbe537be283ced5dbb621f412
seunghun <dau4723@gmail.com>
2022-09-08 19:45:08 +0900

    item7

---
+++ effective-java/src/main/java/item07/Stack.java
@@ -0,0 +1,35 @@
+package item07;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+	private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+	private Object[] elements;
+	private int size = 0;
+
+	public Stack() {
+		elements = new Object[DEFAULT_INITIAL_CAPACITY];
+	}
+
+	public void push(Object object) {
+		ensureCapacity();
+		elements[size++] = object;
+	}
+
+	public Object pop() {
+		if (size == 0) {
+			throw new EmptyStackException();
+		}
+		Object result = elements[size];
+		elements[size--] = null;
+		return result;
+	}
+
+	private void ensureCapacity() {
+		if (elements.length == size) {
+			elements = Arrays.copyOf(elements, 2 * size + 1);
+		}
+	}
+}
