2091348958765bf9419812804ff68a1e00aa0c64
seunghun <dau4723@gmail.com>
2023-06-02 09:50:50 +0900

    k8s microservice

---
+++ kubernetes-crash-course/04-currency-exchange-microservice-basic/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    `java-library`
+    id("com.palantir.docker") version "0.35.0"
+    id("org.springframework.boot") version "2.7.12"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:2.1.7.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.1.7.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.1.7.RELEASE")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth:2.1.3.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:2.1.1.RELEASE")
+    implementation("org.springframework.boot:spring-boot-devtools:2.1.7.RELEASE")
+
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("com.sun.xml.bind:jaxb-impl:2.3.1")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+    implementation("javax.activation:activation:1.1.1")
+
+    implementation("com.h2database:h2:1.4.199")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.7.RELEASE")
+}
+
+group = "com.in28minutes.microservices.currencyexchangeservice"
+version = "0.0.1-SNAPSHOT"
+description = "currency-exchange"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    enabled = false
+}
+
+val bootJar = tasks.named("bootJar").get()
+
+tasks.getByName("dockerPrepare", Copy::class) {
+    dependsOn(bootJar)
+    from(bootJar.outputs.files.singleFile)
+    into("build/docker")
+    duplicatesStrategy = DuplicatesStrategy.FAIL
+}
+
+docker {
+    name = "dau47/currency-exchange:$version"
+    dependsOn(bootJar)
+    buildArgs(mapOf("JAR_FILE" to bootJar.outputs.files.singleFile.name))
+    push(true)
+    noCache(true)
+}
