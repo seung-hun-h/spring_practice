d7c38488c3c0d25578158dbdbf4ff1ba59116ac3
seunghun <dau4723@gmail.com>
2023-08-08 09:46:18 +0900

    feat: self validator 추가

--- voucher/src/main/java/com/shcorp/voucher/application/port/in/CreatVoucherCommand.java
+++ voucher/src/main/java/com/shcorp/voucher/application/port/in/CreatVoucherCommand.java
@@ -1,9 +1,19 @@
 package com.shcorp.voucher.application.port.in;
 
+import com.shcorp.voucher.domain.SelfValidating;
 import com.shcorp.voucher.domain.VoucherType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
-public record CreatVoucherCommand(
-	VoucherType voucherType,
-	int amount
-) {
+public class CreatVoucherCommand extends SelfValidating<CreatVoucherCommand> {
+	@NotNull
+	private final VoucherType voucherType;
+	@Min(0)
+	private final int amount;
+
+	public CreatVoucherCommand(VoucherType voucherType, int amount) {
+		this.voucherType = voucherType;
+		this.amount = amount;
+		super.validateSelf();
+	}
 }
---
+++ voucher/src/main/java/com/shcorp/voucher/domain/SelfValidating.java
@@ -0,0 +1,32 @@
+package com.shcorp.voucher.domain;
+
+import java.util.Set;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+public abstract class SelfValidating<T> {
+
+  private final Validator validator;
+
+  protected SelfValidating() {
+    try(ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      validator = factory.getValidator();
+    }
+  }
+
+  /**
+   * Evaluates all Bean Validations on the attributes of this
+   * instance.
+   */
+  @SuppressWarnings("unchecked")
+  protected void validateSelf() {
+    Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+    if (!violations.isEmpty()) {
+      throw new ConstraintViolationException(violations);
+    }
+  }
+}
