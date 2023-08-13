37f68e467cb777ab2088d9e77fe45db8c9a5dbdf
seunghun <dau4723@gmail.com>
2023-08-14 07:10:13 +0900

    refactor: SignUp SelfValidation

--- voucher/src/main/java/com/shcorp/voucher/user/application/port/in/SignUpCommand.java
+++ voucher/src/main/java/com/shcorp/voucher/user/application/port/in/SignUpCommand.java
@@ -2,10 +2,27 @@ package com.shcorp.voucher.user.application.port.in;
 
 import java.time.LocalDateTime;
 
-public record SignUpCommand(
-	String email,
-	String nickname,
-	String password,
-	LocalDateTime requestAt
-) {
+import com.shcorp.voucher.user.domain.SelfValidating;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class SignUpCommand extends SelfValidating<SignUpCommand> {
+	@Email
+	@NotBlank
+	private final String email;
+	@NotBlank
+	private final String nickname;
+	@NotBlank
+	private final String password;
+	@NotNull
+	private final LocalDateTime requestAt;
+
+	public SignUpCommand(String email, String nickname, String password, LocalDateTime requestAt) {
+		this.email = email;
+		this.nickname = nickname;
+		this.password = password;
+		this.requestAt = requestAt;
+		this.validateSelf();
+	}
 }
